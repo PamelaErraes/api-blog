ece47f6b579b3a69225a2148c1e68987f8e9224a
Pamela Erraes <pamela_erraez@hotmail.com>
2025-06-13 19:57:56 -0500

    Rutas relacionadas con autores

--- src/routes/api/autores.routes.js
+++ src/routes/api/autores.routes.js
@@ -2,7 +2,7 @@
 const router = require('express').Router();
 
 const { getAll, create,  } = require('../../controllers/autores.controller');
-// Define your routes here
+
 
 //GET /api/autores
 router.get('/', getAll);
