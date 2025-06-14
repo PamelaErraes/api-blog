c65366db724c97f196770014f748acabdad15340
Pamela Erraes <pamela_erraez@hotmail.com>
2025-06-13 20:19:37 -0500

    Configura la conexión a la base de datos MySQL

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
+
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_NAME } = process.env;
 const pool =mysql.createPool({
     host: DB_HOST,
